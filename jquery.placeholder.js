93eca18b58b15c5ba784f822f30510484023a069
Tim McCall <mccalltd@gmail.com>
2012-09-24 22:11:41 -0600

    fixed tabs/spaces, etc.

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -5,13 +5,13 @@
  * Copyright 2012 Tim McCall and all other contributors
  * Released under the MIT license
  */
- (function ($) {
+(function ($) {
 
-	var nativeSupport = 'placeholder' in document.createElement('input');
+    var nativeSupport = 'placeholder' in document.createElement('input');
 
-	/***************************************************************
+    /***************************************************************
 	 * Placeholder class definition
-     ***************************************************************/
+	 ***************************************************************/
 
     var Placeholder = function (element, settings) {
         this._visible = false;
@@ -23,14 +23,14 @@
     };
 
     Placeholder.prototype = {
-
-		// Build an label to lay over the element; 
-		// this label will provide mock placeholder functionality.
+        
+        // Build a label to lay over the element; 
+        // this label will provide mock placeholder functionality.
         _buildOverlabel: function () {
             var $element = this.$element,
-				$overlabel = null,
+                $overlabel,
                 elementFloat = $element.css('float'),
-				elementPosition = $element.css('position');
+                elementPosition = $element.css('position');
 
             $overlabel = this.$overlabel = $('<span>')
                 .hide()
@@ -47,30 +47,30 @@
                     'font-family': $element.css('font-family'),
                     'line-height': $element.css('line-height')
                 });
-				
-			if (elementFloat !== 'none') {
-                $overlabel.css({
-					'float': elementFloat,
-					'position': 'static'
-				});
 
-			} else { // elementFloat === 'none'
+            if (elementFloat !== 'none') {
+                $overlabel.css({
+                    'float': elementFloat,
+                    'position': 'static'
+                });
+
+            } else { // elementFloat === 'none'
                 $overlabel.css({ 'position': 'absolute' });
-				
-				if (elementPosition === 'absolute') {
-					$overlabel.css({
-						top: $element.css('top'),
-						left: $element.css('left'),
-						'margin-left': 'auto'
-					});
-				}
-			}
-			
-			$overlabel.insertAfter($element);
+
+                if (elementPosition === 'absolute') {
+                    $overlabel.css({
+                        top: $element.css('top'),
+                        left: $element.css('left'),
+                        'margin-left': 'auto'
+                    });
+                }
+            }
+
+            $overlabel.insertAfter($element);
         },
 
-		// For the given element and collection of property names, 
-		// will sum the parsed float values of each css property on the element.
+        // For the given element and collection of property names, 
+        // will sum the parsed float values of each css property on the element.
         _sumCssProperties: function ($element, cssProperties) {
             var sum = 0;
             $.each(cssProperties, function (index, property) {
@@ -79,39 +79,39 @@
             return sum + 'px';
         },
 
-		// Sets focus on the element.
+        // Sets focus on the element.
         focus: function () {
             this.$element.focus();
         },
 
-		// Hides the placeholder.
+        // Hides the placeholder.
         hide: function () {
             if (!this._visible) return;
             this.$overlabel.hide();
             this._visible = false;
         },
 
-		// Initializes the placeholder functionality.
+        // Initializes the placeholder functionality.
         init: function () {
             this._buildOverlabel();
             this.wireup();
             this.toggle();
         },
 
-		// Shows the placeholder label.
+        // Shows the placeholder label.
         show: function () {
-            if (this._visible) return; 
+            if (this._visible) return;
             this.$overlabel.show();
             this._visible = true;
         },
 
-		// Toggles the visibility of the placeholder, 
-		// depending on the current value of the element.
+        // Toggles the visibility of the placeholder, 
+        // depending on the current value of the element.
         toggle: function () {
             (this.element.value === '') ? this.show() : this.hide();
         },
 
-		// Registers event handlers on the element and placeholder label.
+        // Registers event handlers on the element and placeholder label.
         wireup: function () {
             this.$overlabel
                 .on('click.placeholder', $.proxy(this.focus, this));
@@ -125,28 +125,28 @@
     /***************************************************************
 	 * Plugin definition
      ***************************************************************/
-    
-	$.fn.placeholder = nativeSupport ? $.noop : function (option) {
-		// Filter by elements with placeholder attributes.
-		return this.filter('[placeholder]').each(function () {
 
-			var $this = $(this),
-				data = $this.data('placeholder'),
-				options = $.extend({}, $.fn.placeholder.defaults, typeof option === 'object' ? option : {});
+    $.fn.placeholder = nativeSupport ? $.noop : function (option) {
+        // Filter by elements with placeholder attributes.
+        return this.filter('[placeholder]').each(function () {
 
-			// Initialize only once.
-			if (!data) $this.data('placeholder', (data = new Placeholder(this, options)));
+            var $this = $(this),
+                data = $this.data('placeholder'),
+                options = $.extend({}, $.fn.placeholder.defaults, typeof option === 'object' ? option : {});
 
-			// Call method on placeholder object attached to this element.
-			if (typeof option === 'string') data[option]();
+            // Initialize only once.
+            if (!data) $this.data('placeholder', (data = new Placeholder(this, options)));
 
-		}).end(); // Remove filter so chaining works for the caller.
-	};
+            // Call method on placeholder object attached to this element.
+            if (typeof option === 'string') data[option]();
 
-	$.fn.placeholder.nativeSupport = nativeSupport;
-	
+        }).end(); // Remove filter so chaining works for the caller.
+    };
+
+    $.fn.placeholder.nativeSupport = nativeSupport;
+
     $.fn.placeholder.defaults = {
         overlabelClass: 'placeholder'
     };
 
-} (jQuery));
+}(jQuery));
